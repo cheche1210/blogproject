6d4a3f3e7ed7958166b908263e8f5607310446c5
cheche1210 <invictuscheche@gmail.com>
2019-07-28 16:25:35 +0900

    third commit

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'u!%v%bd$)i@9pli3j0ywdj6jh7tf8d2tnh4y183wji8srz$h$('
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'u!%v%bd$)i@9pli3j0ywdj6jh7tf8d2tnh4y183wji8srz$h$(')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
